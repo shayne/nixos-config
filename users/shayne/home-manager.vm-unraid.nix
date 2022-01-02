1bb44dd048401a20e37df90132b50ef6b80560fd
shayne <79330+shayne@users.noreply.github.com>
2022-01-01 20:02:50 -0500

    add openvscode-server

--- users/shayne/home-manager.vm-unraid.nix
+++ users/shayne/home-manager.vm-unraid.nix
@@ -1,7 +1,27 @@
-{ ... }: {
+{ pkgs, config, lib, ... }: {
+
   imports = [
     ./home-manager-shared.nix
   ];
 
+  home.packages = [
+    pkgs.openvscode-server
+  ];
+
   services.gpg-agent.pinentryFlavor = "tty";
+
+  systemd.user.services."openvscode-server" = {
+    Unit.Description = "OpenVSCode Server";
+    Install.WantedBy = [ "default.target" ];
+    Service = {
+      Environment = "PATH=${config.home.profileDirectory}/bin:/run/wrappers/bin:/run/current-system/sw/bin";
+      /*${
+        lib.makeBinPath
+        (with pkgs; [ coreutils findutils gnugrep gnused systemd ])
+      }";*/
+      ExecStart = "${pkgs.openvscode-server}/bin/openvscode-server --connectionToken q9UCNzUb";
+      Restart = "always";
+    };
+  };
 }
+
