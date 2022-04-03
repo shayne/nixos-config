2d4f2ee87492352ed3e6ca9282579b216da3ace7
shayne <79330+shayne@users.noreply.github.com>
2022-04-02 22:57:43 -0400

    update nixos-vscode-server

--- users/shayne/home-manager.wsl2-amd64.nix
+++ users/shayne/home-manager.wsl2-amd64.nix
@@ -2,7 +2,7 @@
 
   imports = [
     ./home-manager-shared.nix
-    "${fetchTarball { url = "https://github.com/msteen/nixos-vscode-server/tarball/master"; sha256 = "00aqwrr6bgvkz9bminval7waxjamb792c0bz894ap8ciqawkdgxp";}}/modules/vscode-server/home.nix"
+    "${fetchTarball { url = "https://github.com/msteen/nixos-vscode-server/tarball/master"; sha256 = "0pvnf5cya0lh1aggrj891xbzyh06jg8mixjl4zn6a1cwr23380qv";}}/modules/vscode-server/home.nix"
   ];
 
   services.vscode-server.enable = true;
