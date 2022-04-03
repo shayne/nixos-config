9a227565365db9fb756acb2a8b25af217d7b8a9c
shayne <79330+shayne@users.noreply.github.com>
2022-04-03 10:23:11 -0400

    remove vscode-server module from wsl2 specific config

--- users/shayne/home-manager.wsl2-amd64.nix
+++ users/shayne/home-manager.wsl2-amd64.nix
@@ -2,7 +2,6 @@
 
   imports = [
     ./home-manager-shared.nix
-    "${fetchTarball { url = "https://github.com/msteen/nixos-vscode-server/tarball/master"; sha256 = "0pvnf5cya0lh1aggrj891xbzyh06jg8mixjl4zn6a1cwr23380qv";}}/modules/vscode-server/home.nix"
   ];
 
 }
