0aa4ceffafb64e54c6c0905c769e6c328a04d71b
shayne <79330+shayne@users.noreply.github.com>
2022-04-16 10:06:32 -0400

    remove go_1_17 overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -8,9 +8,4 @@ final: prev: {
   terraform-bin = final.callPackage ../pkgs/terraform-bin.nix {};
 
   caddy = final.callPackage ../pkgs/caddy.nix {};
-
-  # Have to force Go 1.17 because the default is fixed to 1.16
-  # for reasons in the nixpkgs repository. We'll undo this when
-  # they switch.
-  go = final.go_1_17;
 }
