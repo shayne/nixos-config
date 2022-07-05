fb555f8fd33e35b321f76005b239b19e14ac9fe0
shayne <79330+shayne@users.noreply.github.com>
2022-07-04 22:38:28 -0400

    update to 22.05

--- flake.nix
+++ flake.nix
@@ -5,13 +5,13 @@
     # Pin our primary nixpkgs repository. This is the main nixpkgs repository
     # we'll use for our configurations. Be very careful changing this because
     # it'll impact your entire system.
-    nixpkgs.url = "github:nixos/nixpkgs/release-21.11";
+    nixpkgs.url = "github:nixos/nixpkgs/release-22.05";
 
     # We use the unstable nixpkgs repo for some packages.
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-21.11";
+      url = "github:nix-community/home-manager/release-22.05";
 
       # We want home-manager to use the same set of nixpkgs as our system.
       inputs.nixpkgs.follows = "nixpkgs";
--- users/shayne/home-manager-shared.nix
+++ users/shayne/home-manager-shared.nix
@@ -3,7 +3,7 @@
 let sources = import ../../nix/sources.nix; in {
   imports = [
     ../../secret/modules/ssh.nix
-    "${fetchTarball { url = "https://github.com/msteen/nixos-vscode-server/tarball/master"; sha256 = "1cszfjwshj6imkwip270ln4l1j328aw2zh9vm26wv3asnqlhdrak";}}/modules/vscode-server/home.nix"
+    "${fetchTarball { url = "https://github.com/msteen/nixos-vscode-server/tarball/master"; sha256 = "0a62zj4vlcxjmn7a30gkpq3zbfys3k1d62d9nn2mi42yyv2hcrm1";}}/modules/vscode-server/home.nix"
   ];
 
 
@@ -88,7 +88,6 @@ let sources = import ../../nix/sources.nix; in {
     enable = true;
     nix-direnv = {
         enable = true;
-        enableFlakes = true;
     };
     config = {
       whitelist = {
