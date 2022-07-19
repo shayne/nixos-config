dbd25454faaddeba7e1b267a51d471544e9cc516
shayne <79330+shayne@users.noreply.github.com>
2022-07-19 12:02:00 -0400

    Update m1 to latest Asahi release/kernel

--- users/shayne/home-manager-shared.nix
+++ users/shayne/home-manager-shared.nix
@@ -3,7 +3,7 @@
 let sources = import ../../nix/sources.nix; in {
   imports = [
     ../../secret/modules/ssh.nix
-    "${fetchTarball { url = "https://github.com/msteen/nixos-vscode-server/tarball/master"; sha256 = "0a62zj4vlcxjmn7a30gkpq3zbfys3k1d62d9nn2mi42yyv2hcrm1";}}/modules/vscode-server/home.nix"
+    "${fetchTarball { url = "https://github.com/msteen/nixos-vscode-server/tarball/master"; sha256 = "1dr3v3mlf61nrs3f3d9qx74y8v5jihkk8wd1li4sglx22aqh4xf6";}}/modules/vscode-server/home.nix"
   ];
 
 
