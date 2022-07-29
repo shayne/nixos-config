a95f365bee3e6b1525b72507fa7c714653f0c854
shayne <79330+shayne@users.noreply.github.com>
2022-07-28 23:47:22 -0400

    remove alacritty from macbook

--- users/shayne/home-manager.macbook.nix
+++ users/shayne/home-manager.macbook.nix
@@ -25,23 +25,6 @@
       };
   };
 
-  programs.alacritty = {
-    enable = true;
-
-    settings = {
-      env.TERM = "xterm-256color";
-
-      key_bindings = [
-        { key = "K"; mods = "Command"; chars = "ClearHistory"; }
-        { key = "V"; mods = "Command"; action = "Paste"; }
-        { key = "C"; mods = "Command"; action = "Copy"; }
-        { key = "Key0"; mods = "Command"; action = "ResetFontSize"; }
-        { key = "Equals"; mods = "Command"; action = "IncreaseFontSize"; }
-        { key = "Subtract"; mods = "Command"; action = "DecreaseFontSize"; }
-      ];
-    };
-  };
-
   programs.kitty = {
     enable = true;
     extraConfig = builtins.readFile ./kitty;
