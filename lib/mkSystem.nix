37243a2a8dd2731d37b1a5aeee957923753318ed
shayne <79330+shayne@users.noreply.github.com>
2022-08-16 01:26:26 +0000

    Add currentSystemName and currentSystem (arch) to system modules

--- lib/mkSystem.nix
+++ lib/mkSystem.nix
@@ -5,7 +5,12 @@ with inputs;
 
 {name, system, modules ? [] }: 
 
-{
+let
+  args = {
+    currentSystemName = name;
+    currentSystem = system;
+  };
+in {
   ${name} = nixosSystem {
     inherit system;
 
@@ -20,12 +25,16 @@ with inputs;
       home-manager.nixosModules.home-manager {
         home-manager.useGlobalPkgs = true;
         home-manager.useUserPackages = true;
+        home-manager.extraSpecialArgs = args;
         home-manager.users.${user} = lib.mkMerge [
             (import ../users/${user}/home-manager-shared.nix)
             (import ../users/${user}/home-manager.${name}.nix)
         ];
       }
 
+      {
+        config._module.args = args;
+      }
     ] ++ modules;
   };
 }
