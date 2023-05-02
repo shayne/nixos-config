7b88f7f21e6f29968d3734e98a688a843ae5af22
shayne <79330+shayne@users.noreply.github.com>
2023-05-01 22:18:08 -0400

    Rectangle: add RectangleConfig.json

--- users/shayne/home-manager-shared.nix
+++ users/shayne/home-manager-shared.nix
@@ -87,6 +87,9 @@ in
    xdg.configFile."nvim/queries/proto/textobjects.scm".source =
      ./textobjects.scm;
 
+  # Rectangle.app. This has to be imported manually using the app.
+  xdg.configFile."rectangle/RectangleConfig.json".text = builtins.readFile ./RectangleConfig.json;
+
   #---------------------------------------------------------------------
   # Programs
   #---------------------------------------------------------------------
