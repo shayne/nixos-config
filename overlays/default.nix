6ae36d96063ebe3180ecee973daa216bd2884842
shayne <79330+shayne@users.noreply.github.com>
2023-09-02 20:47:15 -0400

    cleanup

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,10 +2,5 @@
  * other ".nix" files in this directory are automatically loaded.
  */
 final: _prev: {
-  consul-bin = final.callPackage ../pkgs/consul-bin.nix { };
-  create-dmg = final.callPackage ../pkgs/create-dmg.nix { };
-  nomad-bin = final.callPackage ../pkgs/nomad-bin.nix { };
-  terraform-bin = final.callPackage ../pkgs/terraform-bin.nix { };
-
   caddy = final.callPackage ../pkgs/caddy.nix { };
 }
