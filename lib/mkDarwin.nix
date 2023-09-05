0a33969639259b12ab505fff04397a5e6c1fb849
shayne <79330+shayne@users.noreply.github.com>
2023-09-04 20:55:22 -0400

    darwin: fix up (refactor wip)

--- lib/mkDarwin.nix
+++ lib/mkDarwin.nix
@@ -1,49 +1,38 @@
-{ lib, inputs, overlays, user }:
+{ inputs, outputs, user, stateVersion }:
 
-with lib;
-with inputs;
-
-let
-  _overlays = overlays;
-in
-
-{ name, system, overlays ? [ ] }:
+{ name, system }:
 
 let
   args = {
-    inherit user inputs;
+    inherit user;
     currentSystemName = name;
     currentSystem = system;
   };
 in
 {
-  ${name} = nix-darwin.lib.darwinSystem {
+  ${name} = inputs.nix-darwin.lib.darwinSystem {
     inherit system;
 
     modules = [
-
-      { nixpkgs.overlays = _overlays ++ overlays; }
-
-      ../machines/shared.nix
+      ../nix-darwin
       ../machines/${name}.nix
-      ../users/${user}/darwin.nix
 
-      home-manager.darwinModules.home-manager
+      inputs.home-manager.darwinModules.home-manager
       {
         home-manager.useGlobalPkgs = true;
         home-manager.useUserPackages = true;
         home-manager.extraSpecialArgs = args;
-        home-manager.users.${user} = lib.mkMerge [
+        home-manager.users.${user} = inputs.nixpkgs.lib.mkMerge [
           (import ../users/${user}/home-manager-shared.nix)
           (import ../users/${user}/home-manager.${name}.nix)
         ];
       }
-
-      {
-        config._module.args = args;
-      }
     ];
 
-    specialArgs = { inherit inputs; };
+    specialArgs = {
+      inherit inputs outputs stateVersion user;
+      currentSystemName = name;
+      currentSystem = system;
+    };
   };
 }
---
+++ nix-darwin/default.nix
@@ -0,0 +1,147 @@
+{ config, lib, inputs, outputs, pkgs, ... }:
+{
+  # TODO: darwin error: boot does not exist
+  # imports = [
+  #   ../modules/services/tailscale.nix # unstable service override
+  # ];
+
+  # Only install the docs I use
+  documentation.enable = true;
+  documentation.man.enable = true;
+  documentation.info.enable = false;
+  documentation.doc.enable = false;
+
+  environment = {
+    systemPackages = with pkgs; [
+      gitMinimal
+      gnumake
+      home-manager
+      killall
+      niv
+      rsync
+      neovim-nightly
+      wget
+    ];
+    variables = {
+      EDITOR = "nvim";
+      SYSTEMD_EDITOR = "nvim";
+      VISUAL = "nvim";
+    };
+    # https://github.com/nix-community/home-manager/pull/2408
+    pathsToLink = [ "/share/fish" ];
+  };
+
+  fonts = {
+    fontDir.enable = true;
+    fonts = with pkgs; [
+      (nerdfonts.override { fonts = [ "FiraCode" "SourceCodePro" "UbuntuMono" ]; })
+      fira
+      fira-go
+      joypixels
+      liberation_ttf
+      noto-fonts-emoji
+      source-serif
+      ubuntu_font_family
+      work-sans
+    ];
+  };
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+      inputs.neovim-nightly-overlay.overlay
+      inputs.nixos-apple-silicon.overlays.apple-silicon-overlay
+
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+
+      # TODO: don't do this
+      (import ../users/shayne/vim.nix)
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+      # Accept the joypixels license
+      joypixels.acceptLicense = true;
+    };
+  };
+
+  nix = {
+    gc = {
+      automatic = true;
+      options = "--delete-older-than 10d";
+    };
+
+    # This will add each flake input as a registry
+    # To make nix3 commands consistent with your flake
+    registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
+
+    # This will additionally add your inputs to the system's legacy channels
+    # Making legacy nix commands consistent as well, awesome!
+    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
+
+    package = pkgs.unstable.nix;
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = [ "nix-command" "flakes" ];
+
+      # Avoid unwanted garbage collection when using nix-direnv
+      keep-outputs = true;
+      keep-derivations = true;
+
+      warn-dirty = false;
+    };
+  };
+
+  programs.fish.enable = true;
+
+  homebrew = {
+    enable = true;
+    brews = [
+      "lima"
+    ];
+    casks = [
+      # "1password"
+      # "alfred"
+      "amethyst"
+      "cleanshot"
+      "discord"
+      # "google-chrome"
+      # "imageoptim"
+      "istat-menus"
+      "maccy"
+      # "monodraw"
+      "raycast"
+      "rectangle"
+      # "screenflow"
+      # "slack"
+      "signal"
+      "spotify"
+      "syncthing"
+    ];
+  };
+
+  # The user should already exist, but we need to set this up so Nix knows
+  # what our home directory is (https://github.com/LnL7/nix-darwin/issues/423).
+  users.users.shayne = {
+    home = "/Users/shayne";
+    shell = pkgs.fish;
+  };
+
+  system.activationScripts.diff = {
+    supportsDryActivation = true;
+    text = ''
+      ${pkgs.nvd}/bin/nvd --nix-bin-dir=${pkgs.nix}/bin diff /run/current-system "$systemConfig"
+    '';
+  };
+}
