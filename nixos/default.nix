c69d3b1af03531ab3022f9079cff0376cf130d3e
shayne <79330+shayne@users.noreply.github.com>
2023-09-06 22:07:15 -0400

    nixos/default.nix: add boot attrs

--- nixos/default.nix
+++ nixos/default.nix
@@ -4,6 +4,22 @@
     ../modules/services/tailscale.nix # unstable service override
   ];
 
+  boot = {
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+    kernelParams = [
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
+    kernel.sysctl = {
+      "net.ipv4.ip_forward" = 1;
+      "net.ipv6.conf.all.forwarding" = 1;
+    };
+  };
+
   console = {
     font = "${pkgs.tamzen}/share/consolefonts/TamzenForPowerline10x20.psf";
     packages = with pkgs; [ tamzen ];
