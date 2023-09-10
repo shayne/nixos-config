df3fc0d09bb9c403eb368a15df3b75e9be0ea33e
shayne <79330+shayne@users.noreply.github.com>
2023-09-10 15:37:06 -0400

    remove unused systems from `forAllSystems`

--- lib/default.nix
+++ lib/default.nix
@@ -3,9 +3,7 @@
   loadSystems = import ./loadSystems.nix { inherit inputs outputs user stateVersion; };
   forAllSystems = inputs.nixpkgs.lib.genAttrs [
     "aarch64-linux"
-    "i686-linux"
     "x86_64-linux"
     "aarch64-darwin"
-    "x86_64-darwin"
   ];
 }
