962b5f4154d61f857827a455f12389f7b8a1d114
shayne <79330+shayne@users.noreply.github.com>
2023-10-31 21:47:22 -0400

    pkgs/caddy: update sha256

--- pkgs/caddy.nix
+++ pkgs/caddy.nix
@@ -3,7 +3,7 @@ pkgs.stdenv.mkDerivation {
   name = "caddy";
   src = pkgs.fetchurl {
     url = "https://caddyserver.com/api/download?os=linux&arch=amd64&p=github.com%2Fcaddy-dns%2Fcloudflare&idempotency=74165303940021";
-    sha256 = "sha256-ooNZOkL0Ns4hMD5qnipHpd4aznG+gCKo7QEqctRosjc=";
+    sha256 = "sha256-cL//RApn0xiKjJulmlPg0hyZSmeXC9cjidfIxPAFTIo=";
     name = "caddy";
   };
   phases = [ "installPhase" "patchPhase" ];
