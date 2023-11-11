93329f868d5521925027acd1b936f90ece90aff1
shayne <79330+shayne@users.noreply.github.com>
2023-11-11 13:38:35 -0500

    hyprland/default.nix: gdm.wayland, add awesome, dconf

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -3,17 +3,27 @@
   services.xserver = {
     enable = true;
     xkbOptions = "ctrl:nocaps";
+
     displayManager = {
       gdm.enable = true;
+      gdm.wayland = true;
       # Keyboard repeat rate
       sessionCommands = ''
         ${pkgs.xorg.xset}/bin/xset r rate 200 40
       '';
     };
+
+    windowManager.awesome.enable = true;
   };
+
+  console.useXkbConfig = true;
 
   programs.hyprland = {
     enable = true;
+    # package = pkgs.unstable.hyprland;
     xwayland.enable = true;
   };
+
+  # GTK themes in Wayland applications
+  programs.dconf.enable = true;
 }
