244c2dbad4cc5e9336e48b1219057d471a1577b3
shayne <79330+shayne@users.noreply.github.com>
2023-12-10 21:37:42 -0500

    *: update nix attrset to fix direnv cache

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -1,2 +1,17 @@
-# TODO: put common home manager config?
-{ }
+{ lib, pkgs, inputs, ... }: {
+  nix = {
+    # This will add each flake input as a registry
+    # To make nix3 commands consistent with your flake
+    registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
+
+    package = lib.mkForce pkgs.unstable.nixVersions.nix_2_18;
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = [ "nix-command" "flakes" ];
+      # Avoid unwanted garbage collection when using nix-direnv
+      keep-outputs = true;
+      keep-derivations = true;
+      warn-dirty = false;
+    };
+  };
+}
