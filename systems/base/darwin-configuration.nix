7678ca4ea7e09539fdae99d53fe7af34613da53a
shayne <79330+shayne@users.noreply.github.com>
2026-03-21 13:02:51 -0400

    systems/base: install mas via Homebrew brews

--- systems/base/darwin-configuration.nix
+++ systems/base/darwin-configuration.nix
@@ -117,6 +117,8 @@
     };
   };
 
+  homebrew.brews = [ "mas" ];
+
   # The user should already exist, but we need to set this up so Nix knows
   # what our home directory is (https://github.com/LnL7/nix-darwin/issues/423).
   system = {
