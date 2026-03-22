62981867cb6066bd2b84cddb824941f494f49d0a
shayne <79330+shayne@users.noreply.github.com>
2026-03-21 23:12:42 -0400

    modules/custom-fonts: decrypt font archive during activation

--- modules/custom-fonts/default.nix
+++ modules/custom-fonts/default.nix
@@ -1,24 +1,24 @@
 { config, lib, pkgs, ... }:
 lib.mkIf pkgs.stdenv.isDarwin (
   let
-    archivePath = config.sops.secrets.custom-fonts-archive.path;
+    encryptedArchive = ../../secrets/custom-fonts.tar.gz;
+    ageKeyFile = config.sops.age.keyFile;
   in
   {
-    sops.secrets.custom-fonts-archive = {
-      sopsFile = ../../secrets/custom-fonts.tar.gz;
-      format = "binary";
-      path = "%r/custom-fonts.tar.gz";
-    };
-
     home.activation.installCustomFonts = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
       font_dir="$HOME/Library/Fonts/HomeManager/custom-fonts"
-      archive=${lib.escapeShellArg archivePath}
       tmp_dir="$(mktemp -d)"
+      unpack_dir="$tmp_dir/unpacked"
+      archive="$tmp_dir/custom-fonts.tar.gz"
       trap 'rm -rf "$tmp_dir"' EXIT
 
       mkdir -p "$font_dir"
-      ${pkgs.gnutar}/bin/tar -xzf "$archive" -C "$tmp_dir"
-      ${pkgs.rsync}/bin/rsync -a --delete "$tmp_dir"/ "$font_dir"/
+      mkdir -p "$unpack_dir"
+      SOPS_AGE_KEY_FILE=${lib.escapeShellArg ageKeyFile} \
+        ${lib.getExe pkgs.sops} decrypt --input-type binary --output-type binary \
+        ${lib.escapeShellArg "${encryptedArchive}"} > "$archive"
+      /usr/bin/tar -xzf "$archive" -C "$unpack_dir"
+      ${pkgs.rsync}/bin/rsync -a --delete "$unpack_dir"/ "$font_dir"/
     '';
   }
 )
