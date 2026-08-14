c04d98a1d8e189983d5f96b071806914ee4a8e4d
shayne <79330+shayne@users.noreply.github.com>
2026-08-13 20:37:10 -0400

    home-manager/m5mbp: add Ghostty config and theme secret

--- home-manager/shayne/m5mbp/default.nix
+++ home-manager/shayne/m5mbp/default.nix
@@ -1,5 +1,32 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
+  home.file."Library/Application Support/com.mitchellh.ghostty/config.ghostty" = {
+    force = true;
+    text = ''
+      theme = light:One Half Light,dark:Dracula PRO
+      # theme = Tomorrow
+
+      font-family = "BerkeleyMono Nerd Font"
+      font-size = 15
+
+      window-width = 100
+      window-height = 30
+
+      window-padding-x = 6
+      window-padding-y = 6
+      window-padding-balance = true
+
+      macos-option-as-alt = true
+
+      keybind = global:cmd+ctrl+t=toggle_quick_terminal
+
+      shell-integration = fish
+
+      cursor-invert-fg-bg = true
+      keybind = shift+enter=text:\n
+    '';
+  };
+
   home.packages = with pkgs; [
     docker-client
     gnugrep
@@ -13,5 +40,12 @@
     shellAliases = {
       tailscale = "/Applications/Tailscale.app/Contents/MacOS/Tailscale";
     };
+  };
+
+  sops.secrets.ghostty_dracula_pro = { };
+
+  xdg.configFile."ghostty/themes/Dracula PRO" = {
+    force = true;
+    source = config.lib.file.mkOutOfStoreSymlink config.sops.secrets.ghostty_dracula_pro.path;
   };
 }
